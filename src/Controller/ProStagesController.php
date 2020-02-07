8dbc6960edd30c89787bc2924e2e5736290ab105
Paul Lafourcade <plafourca004@iutbayonne.univ-pau.fr>
2020-02-07 11:15:18 +0100

    Ajout de formulaires d'ajout et de modif d'entreprise

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -12,6 +12,13 @@ use App\Repository\StageRepository;
 use App\Repository\EntrepriseRepository;
 use App\Repository\FormationRepository;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
 class ProStagesController extends AbstractController
 {
     public function index(StageRepository $repositeryStage)
@@ -54,5 +61,53 @@ class ProStagesController extends AbstractController
         $stages = $repositeryStage->findByFormation($formation);
         
         return $this->render('pro_stages/formation.html.twig', ['stages' => $stages, 'formation' => $formation]);
+    }
+
+    public function ajouterEntreprise(Request $requetteHttp, ObjectManager $manager)
+    {
+        $entreprise = new Entreprise();
+
+        $formulaireEntreprise = $this->createFormBuilder($entreprise)
+            ->add('nom', TextType::class,['label' => 'Nom de l\'entreprise'])
+            ->add('activite', TextareaType::class,['label' => 'Activité de l\'entreprise'])
+            ->add('adresse', TextareaType::class,['label' => 'Adresse de l\'entreprise'])
+            ->add('lienSite', UrlType::class,['label' => 'Lien du site internet'])
+            ->getForm();
+
+        $formulaireEntreprise->handleRequest($requetteHttp);
+
+        if($formulaireEntreprise->isSubmitted())
+        {
+            $manager->persist($entreprise);
+            $manager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('pro_stages/ajouterEntreprise.html.twig', ['form' => $formulaireEntreprise->createView()]);
+    }
+
+    public function modifierEntreprise(Request $requetteHttp, ObjectManager $manager, Entreprise $entreprise)
+    {
+
+
+        $formulaireEntreprise = $this->createFormBuilder($entreprise)
+            ->add('nom', TextType::class,['label' => 'Nom de l\'entreprise'])
+            ->add('activite', TextareaType::class,['label' => 'Activité de l\'entreprise'])
+            ->add('adresse', TextareaType::class,['label' => 'Adresse de l\'entreprise'])
+            ->add('lienSite', UrlType::class,['label' => 'Lien du site internet'])
+            ->getForm();
+
+        $formulaireEntreprise->handleRequest($requetteHttp);
+
+        if($formulaireEntreprise->isSubmitted())
+        {
+            $manager->persist($entreprise);
+            $manager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('pro_stages/modifierEntreprise.html.twig', ['form' => $formulaireEntreprise->createView()]);
     }
 }
